d0d2554262e6a1b37fc4c414a783737ed571a2fd
edn869728-jpg <edn869728@gmail.com>
2026-05-30 02:09:09 +0800

    Initialize Basic config

---
+++ config.js
@@ -0,0 +1,19 @@
+window.ANG_HR_CONFIG = {
+  edition: 'basic',
+  editionName: 'ANG HR Basic',
+  apiBaseUrl: '',
+  defaultPage: 'employee',
+  features: {
+    employeeHome: true,
+    clock: true,
+    leave: true,
+    clockFix: true,
+    preselect: true,
+    upload: true,
+    notices: true,
+    manager: false,
+    salary: false,
+    permissions: false,
+    archive: false
+  }
+};
