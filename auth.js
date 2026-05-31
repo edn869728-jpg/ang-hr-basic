48656998286b4929fecea4c5fd19daad9040a1de
edn869728-jpg <edn869728@gmail.com>
2026-06-01 02:04:17 +0800

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,30 @@
+window.ANG_HR_FEATURES={
+  "clock": 1,
+  "clock_fix": 1,
+  "leave": 1,
+  "salary": 1,
+  "notice": 1,
+  "admin": 1,
+  "review": 1,
+  "people": 1,
+  "schedule": 0,
+  "upload": 0,
+  "admin_schedule": 0,
+  "admin_salary": 1,
+  "settings": 0,
+  "data": 0,
+  "drive": 0,
+  "api": 0,
+  "multi_review": 0,
+  "ios_shortcut": 1,
+  "field_clock": 1,
+  "overtime_clock": 1,
+  "branches": 1,
+  "employees": 5
+};
+window.ANG_HR_PLAN={
+  "edition": "basic",
+  "name": "Basic",
+  "tagline": "五人內微型店家｜單店基礎版",
+  "included": "1 店｜5 人"
+};
